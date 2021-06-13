f1459c3add2c064d9221da7a5d6610933f1723fd
bernardolemos <bernardo.lfccosta@gmail.com>
2021-06-13 16:04:02 +0100

    [FEAT] Added py36 and py37

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 DESCRIPTION = 'Client library to create serverless REST endpoints on shippedbrain.com'
 LONG_DESCRIPTION = open("README.md", "r", encoding="utf-8").read()
 
@@ -22,7 +22,7 @@ setup(name="shippedbrain",
       long_description=LONG_DESCRIPTION,
       long_description_content_type="text/markdown",
       packages=find_packages(),
-      python_requires=">=3.8.0",
+      python_requires=">=3.6.0",
       install_requires=install_requires,
       extras_require=extras_require,
       keywords=['pretrained-models', 'shipped-brain', 'machine-learning', 'artificial-intelligence', 'deploy', 'serve', "serverless", "model-hub", "data-science", 'mlflow'],
